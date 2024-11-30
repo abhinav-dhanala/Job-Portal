3e2e1d05e77e0cbf413a634af4334c4aeaf0d078
abhinav <abhinav200326@gmail.com>
2024-11-30 17:41:30 +0530

    complete landing page UI,User authentication Clerk and integrated with Supabase

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from '@/components/header';
 
 const AppLayout = () => {
-  console.log("AppLayout is rendering");
+  console.log("AppLayout is rendering"); // Debug log
   return (
     <div>
       <div className="grid-background"></div>
-      <Outlet />
+      <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-10 text-center bg-gray-800 mt-10">Made by Abhinav Dhanala</div>
+      
     </div>
   );
 };

1aea9d58a39bb477af4b6bb30429b15ac07255fd
abhinav <abhinav200326@gmail.com>
2024-12-01 20:12:39 +0530

    Implemented job listing page with search filters and dynamic job cards.Improved UI for and data rendering in Job Card

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,20 +1,49 @@
 import React, { useEffect } from 'react';
 import { getJobs } from '@/api/apiJobs';
-import { useSession } from '@clerk/clerk-react';
+import { useSession, useUser } from '@clerk/clerk-react';
 import useFetch from '@/hooks/use-fetch';
+import { useState } from 'react';
+import { BarLoader } from 'react-spinners';
+import JobCard from '@/components/job-card';
 
 
 const JobListing = () => {
 
-  const {fn:fnJobs,data:dataJobs,loading:loadingJobs}=useFetch(getJobs,{});
-  console.log(dataJobs);
-  useEffect(()=>{
-    fnJobs();
+  const [searchQuery,setSearchQuery]=useState("");
+  const [location,setloacation]=useState("");
+  const [company_id,setcompany_id]=useState("");
+  const {isLoaded}=useUser();
 
-  },[])
+  const {fn:fnJobs,data:jobs,loading:loadingJobs}=useFetch(getJobs,{location,company_id,searchQuery});
+  useEffect(()=>{
+    if(isLoaded)fnJobs();
+  },[isLoaded,location,company_id,searchQuery]);
+
+  if(!isLoaded){
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7"/>;
+  }
   
 
-  return <div>Job Listing</div>;
+  return <div>
+    <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">Latest Jobs</h1>
+
+    {/* add filters here */}
+    {loadingJobs && (
+      <BarLoader className="mt-4" width={"100%"} color="#36d7b7"/>
+    )}
+
+    {loadingJobs===false && (
+      <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {jobs?.length?(
+          jobs.map((job)=>{
+            return <JobCard key={job.id} job={job}/>
+          })
+        ):(
+          <div>No Jobs Found</div>
+        )}
+      </div>
+    )}
+  </div>;
 };
 
 export default JobListing;
